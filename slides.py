8f1aaad0e6c469d7348c684f2247e5df995c80e0
Steen Manniche <steen@manniche.net>
2017-01-03 23:48:05 +0100

    Adding a nice scalable box around the text

--- slides.py
+++ slides.py
@@ -4,6 +4,8 @@ import os
 dbeazley in the video from the PyOhio talk found here:
 https://youtu.be/j6VSAsKAj98
 
+Great talk, by the way. Go see it!
+
 Usage is:
 
 >>> from slides import Slide
@@ -24,9 +26,7 @@ screen and print out the formatted text inside an ascii box.
 """
 
 class Slide(object):
-    """
-
-    The `Slide` class works in a REPL, by first clearing the terminal,
+    """The `Slide` class works in a REPL, by first clearing the terminal,
     then printing the next slide object text formatted in a box.
 
     One thing dbeazley did was:
@@ -49,17 +49,55 @@ class Slide(object):
         - use the `textwrap` module to format the box
         - use http://misc.flogisoft.com/bash/tip_colors_and_formatting
           for formatting text to most terms
+    How:
+        - did dbeazley increase the header? I suspect that iTerm,
+          being a *graphical* terminal has some functionality for
+          this, probably along the lines sketched out in
+          http://superuser.com/questions/877010/increase-decrease-font-size-in-iterm2
+
     """
 
-    def __init__(self, text):
+    def __init__(self, heading, text):
         """
         ... text for slide goes here
         """
-        self.text = text
+        self.heading = heading
+        self.text = text.split('\n')
+        self.formatted = self.text#self._format_text()
+        self.ready_text = self._draw_box()
+
+
+    def _format_text(self):
+        pass
+
+    def _split_line(self, line, width):
+        return [line[i:i+width] for i in range(0, len(line), width)]
+
+
+    def _split_msg(self, msg, width):
+        lines = msg
+        split_lines = [self._split_line(line, width-2 ) for line in lines]
+        return [item for sublist in split_lines for item in sublist]
+
+
+    def _draw_box(self, max_width=80):
+        count = len(max(self.text, key=len)) + 2
+
+        if count > max_width:
+            count = max_width
+
+        tb = "+" + "-" * count + "+"
+
+        hdr = "\n| {0} |\n|{1}|".format(self.heading.ljust(count-2), "".ljust(count))
+        msg = "\n".join("| " + x.ljust(count-2) + " |" for x in self._split_msg(self.formatted, count))
+
+        return "{0}{1}\n{2}\n{0}".format(tb, hdr, msg)
+
+
 
     def __repr__(self):
         """
         formatting out goes here
         """
         os.system('clear');
-        return self.text
+        return self.ready_text
